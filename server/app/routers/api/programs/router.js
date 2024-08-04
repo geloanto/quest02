593ef288722e11d255fa7f133dd7ae1013c3cbff
anthony gelormini <gelo2b@outlook.fr>
2024-08-04 14:50:36 +0200

    finish exo 03

---
+++ server/app/routers/api/programs/router.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const router = express.Router();
+
+const { browse } = require("../../../controllers/programActions");
+
+
+router.get("/", browse);
+
+module.exports = router;
--- server/app/routers/api/router.js
+++ server/app/routers/api/router.js
@@ -2,14 +2,17 @@ const express = require("express");
 
 const router = express.Router();
 
-/* ************************************************************************* */
-// Import And Use Routers Here
-/* ************************************************************************* */
+const sayWelcome = (req, res) => {
+  res.send("Welcome to Wild Series !");
+};
 
+router.get("/", sayWelcome);
+
+;
 const itemsRouter = require("./items/router");
+const programsRouter = require("./programs/router");
 
+router.use("/programs", programsRouter)
 router.use("/items", itemsRouter);
-
-/* ************************************************************************* */
 
 module.exports = router;
