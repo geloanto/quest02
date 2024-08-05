577b8ef515c0015adae7b6c748d0122ce1328b5e
anthony gelormini <gelo2b@outlook.fr>
2024-08-05 17:27:47 +0200

    finished quest 05

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -10,3 +10,17 @@ create table item (
   user_id int unsigned not null,
   foreign key(user_id) references user(id)
 );
+CREATE TABLE category (
+    id INT AUTO_INCREMENT PRIMARY KEY not null,
+    name VARCHAR(255) NOT NULL
+);
+CREATE TABLE program (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    synopsis TEXT NOT NULL,
+    poster VARCHAR(255) not null,
+    country VARCHAR(255) not null,
+    year INT not null,
+    category_id INT not null,
+    FOREIGN KEY (category_id) REFERENCES category (id)
+);
